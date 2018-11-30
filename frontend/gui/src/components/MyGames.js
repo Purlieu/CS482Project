8cb3c4a2b050e7cf2446f88d5ad4eb3d890f6153
dtafesse <dtafesse@uwm.edu>
2018-11-30 16:11:48 -0600

    requiresAuthComponent created

--- frontend/gui/src/components/MyGames.js
+++ frontend/gui/src/components/MyGames.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import requireAuth from "./requireAuth";
 
 class MyGames extends Component {
   render() {
@@ -6,4 +9,7 @@ class MyGames extends Component {
   }
 }
 
-export default MyGames;
+export default compose(
+  requireAuth,
+  connect(null)
+)(MyGames);
