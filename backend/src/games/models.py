a17361a5fc1850bf3474f45c60a3ce7a3c8c5ed4
Purlieus <dorsey2@uwm.edu>
2018-12-11 10:59:33 -0600

    Token Auth

--- backend/src/games/models.py
+++ backend/src/games/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Game(models.Model):
-    title = models.CharField(max_length=120)
-    text = models.CharField(max_length=120)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    notes = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    gameid = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.title
+        return self.gameid
