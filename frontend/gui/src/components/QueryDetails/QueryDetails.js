813490b53730aae9732d5b0734b7df4c694c9cfb
Purlieus <dorsey2@uwm.edu>
2018-12-12 13:41:11 -0600

    More Rating Options

--- frontend/gui/src/components/QueryDetails/QueryDetails.js
+++ frontend/gui/src/components/QueryDetails/QueryDetails.js
@@ -22,7 +22,45 @@ const ratings = [
     {
         value: '1',
         label: 'one',
-    }
+    },
+    {
+        value: '2',
+        label: 'two',
+    },
+    {
+        value: '3',
+        label: 'three',
+    },
+    {
+        value: '4',
+        label: 'four',
+    },
+    {
+        value: '5',
+        label: 'five',
+    },
+    {
+        value: '6',
+        label: 'six',
+    },
+    {
+        value: '7',
+        label: 'seven',
+    },
+    {
+        value: '8',
+        label: 'eight',
+    },
+    {
+        value: '9',
+        label: 'nine',
+    },
+    {
+        value: '10',
+        label: 'ten',
+    },
+
+
 ]
 
 class QueryDetails extends Component {
